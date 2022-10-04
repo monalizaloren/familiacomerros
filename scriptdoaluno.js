2a8d82e0f8ff5ab6bbdd6e997bc1424deda4178b
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2022-10-04 17:36:28 -0300

    Add files via upload

---
+++ scriptdoaluno.js
@@ -0,0 +1,19 @@
+var imagens =["???",
+"???", "???"];
+
+var nome = ["???", "???", "???"];
+
+var i = 0;
+function ???()
+{
+    i++;
+    var numerodeparentes = 4
+    if( i > numerodeparentes)
+    {
+        i=0;
+    }
+    var adicionarimagem = ???[i];
+    var adicionarnome = ???[i];
+    document.getElementById("???").src = adicionarimagem;
+    document.getElementById("???").innerHTML = adicionarnome;
+}
